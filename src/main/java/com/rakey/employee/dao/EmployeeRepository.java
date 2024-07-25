ee6c15b770bf78ad1cc2fe23c1c6ea16b06f467b
Rakesh <rakesh.vuyyuru.19@gmail.com>
2024-07-25 07:21:11 +0530

    Add DAO folder with EmployeeRepository

---
+++ src/main/java/com/rakey/employee/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.rakey.employee.dao;
+
+import com.rakey.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
